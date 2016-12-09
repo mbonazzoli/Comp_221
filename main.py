b9c15d6776205a22037cda0bcc2b81cbe2d077fb
Matthew Bonazzoli <mbonazzo@macalester.edu>
2016-12-08 18:29:23 -0600

    added FFT code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-print ("hello world")
-print ("Hey Brett")
-print ("lil bitch")
+'''Location of interface'''
+#Sample
+
+import wave
+import numpy as np
+
+
+wave_reader = wave.open('demo.wav', 'r')
+
+frames = wave_reader.getnframes()
+array = bytearray(wave_reader.readframes(frames))
+
+print 'byte array :'
+for num in array:
+    print num
+print '____________________________________________'
+
+
+fft = np.fft.fft(array)
+
+
+print fft
+
+
